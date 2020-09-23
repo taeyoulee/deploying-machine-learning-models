bd53b7f9e55693188d3059033a2c5c4894961ec1
taeyoulee <taeyoulee@gmail.com>
2020-09-24 00:29:23 +0800

    update kaggle script

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -2,5 +2,3 @@
 
 kaggle competitions download -c house-prices-advanced-regression-techniques -p packages/regression_model/regression_model/datasets/
 unzip packages/regression_model/regression_model/datasets/house-prices-advanced-regression-techniques.zip -d packages/regression_model/regression_model/datasets/
-mv packages/regression_model/regression_model/datasets/house-prices-advanced-regression-techniques/train.csv packages/regression_model/regression_model/datasets/train.csv 
-mv packages/regression_model/regression_model/datasets/house-prices-advanced-regression-techniques/test.csv packages/regression_model/regression_model/datasets/test.csv 
